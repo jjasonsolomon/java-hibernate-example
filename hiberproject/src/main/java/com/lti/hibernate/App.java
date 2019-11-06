15e67fbf64f30de3fb0c60f9dfd922d124e0d23c
jjasonsolomon <jasonsolomon.j@gmail.com>
2019-11-06 15:58:45 +0530

    commit app changes

---
+++ hiberproject/src/main/java/com/lti/hibernate/App.java
@@ -0,0 +1,25 @@
+package com.lti.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+public static void main(String...args){
+	Configuration config=new Configuration().configure();
+	StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(config.getProperties());
+	
+	SessionFactory factory =config.buildSessionFactory(builder.build());
+	Session session = factory.openSession();
+	Employee emp = new Employee();
+	emp.setId(1);
+	emp.setEmpName("Jas");
+	emp.setBranch("TSI");
+	session.beginTransaction();
+	session.save(emp);
+	session.getTransaction().commit();
+	
+	
+}
+}
